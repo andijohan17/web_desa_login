dbf3866346a176509de75db1df2d6aac6d40f083
andijohan17 <162283801+andijohan17@users.noreply.github.com>
2024-07-19 16:30:55 +0700

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        customTeal: '#336F6C',
+        bebas: '#69BCB8',
+        login: '#336F6C',
+        akun: '#F5F5F5',
+        hijau: '#336F6C',
+      },
+    },
+  },
+  plugins: [],
+}
+
